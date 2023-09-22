2a2d39cb04893ac5b6b91e3f2936281cce8a6bde
joywin2003 <21a20.joywin@sjec.ac.in>
2023-09-22 16:34:59 +0530

    fixed sc error by using subprocess

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import speech_recognition as sr
 import os
+import subprocess
 
 def say(text):
-    os.system("say " + text)
+    # Escape the text properly and use subprocess to execute the say command
+    subprocess.run(["say", text])
 
 def takeCommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         r.pause_threshold = 1
         audio = r.listen(source)
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}")
-        return query
-
+        try:
+            query = r.recognize_google(audio, language='en-in')
+            print(f"User said: {query}")
+            return query
+        except Exception as e:
+            return "Some error has occurred, Sorry Joywin"
 
 if __name__ == '__main__':
-    say("Hello, my name is Jarvis. What can I do for you?")
-    print("Listening...")
-    text = takeCommand()
-    say(text)
+    say("Hello, my name is J.A.R.V.I.S. What can I do for you?")
+    while True:
+        print("Listening...")
+        text = takeCommand()
+        say(text)
