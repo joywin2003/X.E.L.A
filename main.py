836787b5a65120579c5759eba0ee4938ab3a5b71
joywin2003 <21a20.joywin@sjec.ac.in>
2023-09-22 20:13:06 +0530

    opening site functionality

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import speech_recognition as sr
 import os
 import subprocess
+import webbrowser
+
 
 def say(text):
-    # Escape the text properly and use subprocess to execute the say command
     subprocess.run(["say", text])
+
 
 def takeCommand():
     r = sr.Recognizer()
@@ -18,9 +20,31 @@ def takeCommand():
         except Exception as e:
             return "Some error has occurred, Sorry Joywin"
 
+
 if __name__ == '__main__':
-    say("Hello, my name is J.A.R.V.I.S. What can I do for you?")
+    say("Hey Joywin, what can I do for you today?")
     while True:
         print("Listening...")
-        text = takeCommand()
-        say(text)
+        query = takeCommand()
+        sites = {
+            "Google": "https://www.google.com",
+            "Facebook": "https://www.facebook.com",
+            "YouTube": "https://www.youtube.com",
+            "Twitter": "https://www.twitter.com",
+            "Instagram": "https://www.instagram.com",
+            "LinkedIn": "https://www.linkedin.com",
+            "Amazon": "https://www.amazon.com",
+            "Netflix": "https://www.netflix.com",
+            "GitHub": "https://www.github.com",
+            "Reddit": "https://www.reddit.com",
+            "Wikipedia": "https://www.wikipedia.org",
+            "Stack Overflow": "https://stackoverflow.com",
+            "Yahoo": "https://www.yahoo.com",
+            "Apple": "https://www.apple.com",
+            "Spotify": "https://www.spotify.com",
+            "WhatsApp": "https://www.whatsapp.com",
+        }
+        for site, url in sites.items():
+            if f"Open {site}".lower() in query.lower():
+                say(f"Opening {site}...")
+                webbrowser.open(url)
