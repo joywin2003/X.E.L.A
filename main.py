28f60d52ccdec258fe984824b5a52bf0a9c5fbf1
joywin2003 <21a20.joywin@sjec.ac.in>
2023-09-22 16:21:32 +0530

    added listening command

--- main.py
+++ main.py
@@ -4,5 +4,18 @@ import os
 def say(text):
     os.system("say " + text)
 
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}")
+        return query
+
+
 if __name__ == '__main__':
-    say("Hey Mansur Bhai, how are you doing with your girlfriend?")
+    say("Hello, my name is Jarvis. What can I do for you?")
+    print("Listening...")
+    text = takeCommand()
+    say(text)
